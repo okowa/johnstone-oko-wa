518154c88a17ad2a769d9495ea365a36964942b4
Johnstone okowa <johnstone-okowa@Johnstones-Mac-mini.local>
2021-03-04 23:45:33 +0100

    Final Change

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/layout/Header'
 import HomePage from "./pages/HomePage";
